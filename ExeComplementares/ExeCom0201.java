0d00ea67347afaac5ca81e3437a78db4d7419316
Guilherme Oliveira <78177891+gloliveira1701@users.noreply.github.com>
2026-03-25 17:54:51 -0300

    Add ExeCom0201.java with movie details

---
+++ ExeComplementares/ExeCom0201.java
@@ -0,0 +1,17 @@
+public class ExeCom0201 {
+    public static void main(String[] args) {
+        String filme = "Vingadores";
+        int ano = 2015;
+        int duracao = 240;
+        float nota = 8.7f;
+        char inicial = 'V';
+        boolean sucesso = true;
+
+        System.out.println(filme);
+        System.out.println(ano);
+        System.out.println(duracao);
+        System.out.println(nota);
+        System.out.println(inicial);
+        System.out.println(sucesso);
+    }
+}
