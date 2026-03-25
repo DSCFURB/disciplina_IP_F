90040864704f7cdb60b4bbbe5468c38e51ecf23b
Guilherme Oliveira <78177891+gloliveira1701@users.noreply.github.com>
2026-03-25 17:54:08 -0300

    Add ExeCom0103.java with game data display

---
+++ ExeComplementares/ExeCom0103.java
@@ -0,0 +1,13 @@
+public class ExeCom0103 {
+    public static void main(String[] args) {
+        byte jogoIndie = 100;
+        short jogoNacional = 32000;
+        int jogoAAA = 2000000000;
+        long totalHistorico = 50000000000L;
+
+        System.out.println("Indie: " + jogoIndie);
+        System.out.println("Nacional: " + jogoNacional);
+        System.out.println("AAA: " + jogoAAA);
+        System.out.println("Total: " + totalHistorico);
+    }
+}
