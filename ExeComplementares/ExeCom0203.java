7ca31bc325aa0a1969c087469dadb45bab779288
Guilherme Oliveira <78177891+gloliveira1701@users.noreply.github.com>
2026-03-25 17:58:01 -0300

    Add ExeCom0203.java with basic introduction program

---
+++ ExeComplementares/ExeCom0203.java
@@ -0,0 +1,9 @@
+public class ExeCom0203 {
+    public static void main(String[] args) {
+        String nome = "Ana";
+        int idade = 25;
+        String hobby = "desenhar";
+
+        System.out.println("Olá, meu nome é " + nome + ", tenho " + idade + " anos e gosto de " + hobby + ".");
+    }
+}
