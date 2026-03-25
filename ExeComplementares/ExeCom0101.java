16d508f3e7497680dfd457c49944059ce9c94b4c
Guilherme Oliveira <78177891+gloliveira1701@users.noreply.github.com>
2026-03-25 17:53:35 -0300

    Add ExeCom0101.java with basic user info display

---
+++ ExeComplementares/ExeCom0101.java
@@ -0,0 +1,13 @@
+public class ExeCom0101 {
+    public static void main(String[] args) {
+        String nome = "Guilherme";
+        int idade = 30;
+        char inicial = 'G';
+        boolean gostaCafe = true;
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Inicial: " + inicial);
+        System.out.println("Gosta de café? " + gostaCafe);
+    }
+}
