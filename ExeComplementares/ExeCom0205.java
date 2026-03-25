3faad53e4ff34ea21372c5b05b6c73e49fe9d5fb
Guilherme Oliveira <78177891+gloliveira1701@users.noreply.github.com>
2026-03-25 17:58:15 -0300

    Add ExeCom0205.java to replace placeholder name

---
+++ ExeComplementares/ExeCom0205.java
@@ -0,0 +1,7 @@
+public class ExeCom0205 {
+    public static void main(String[] args) {
+        String fraseOriginal = "Eu ALUNO XXXXX, vou aprender Java! Java não é difícil!";
+        String fraseFinal = fraseOriginal.replace("ALUNO XXXXX", "Guilherme");
+        System.out.println(fraseFinal);
+    }
+}
