0abdbce2df70fb3faf71d7573a7bcee033bbbbac
Guilherme Oliveira <78177891+gloliveira1701@users.noreply.github.com>
2026-03-25 17:53:49 -0300

    Add ExeCom0102.java with character class example

---
+++ ExeComplementares/ExeCom0102.java
@@ -0,0 +1,14 @@
+public class ExeCom0102 {
+    public static void main(String[] args) {
+        int idade = 25;
+        float altura = 1.75f;
+        char classe = 'G'; // G de Guerreiro
+        boolean emMissao = false;
+
+        System.out.println("Personagem Fictício:");
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("Inicial da Classe: " + classe);
+        System.out.println("Está em missão? " + emMissao);
+    }
+}
