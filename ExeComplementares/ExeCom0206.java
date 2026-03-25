d200529570ebcabd28106f3a5a0083d1fef150f6
Guilherme Oliveira <78177891+gloliveira1701@users.noreply.github.com>
2026-03-25 17:58:27 -0300

    Add ExeCom0206.java to print song lyrics

---
+++ ExeComplementares/ExeCom0206.java
@@ -0,0 +1,11 @@
+public class ExeCom0206 {
+    public static void main(String[] args) {
+        String l1 = "Posso pressentir o perigo e o caos\n";
+        String l2 = "tu ru ruru\n";
+        String l3 = "E ninguém agora vai me amedrontar\n";
+        String l4 = "tu ru ruru";
+        
+        String letraCompleta = l1 + l2 + l3 + l4;
+        System.out.println(letraCompleta);
+    }
+}
