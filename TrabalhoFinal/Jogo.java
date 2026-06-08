261ab3789aad26e9178630dfba22396015c3f71b
Laura Möller <163305319+LauraMoller@users.noreply.github.com>
2026-06-08 20:38:49 -0300

    Create Jogo.java

---
+++ TrabalhoFinal/Jogo.java
@@ -0,0 +1,24 @@
+public class Jogo {
+    
+    public Jogo() {
+        String[] tesouros = new String[15];
+        //.....
+        iniciar();
+    }
+
+    private String sortear() {
+        String[] tipos = {"VAZIO", "OURO", "DIAMANTE","RUBI", "BURADO", "COBRA", "ESPINHOS"};
+        /*tipos[0]= "VAZIO";
+        tipos[1] = "OURO";
+        tipos[2] = "DIAMANTE";*/
+
+        int posicao = (int)(Math.random() * 7);
+        //System.out.println(posicao + " " + tipos[posicao]);
+        return tipos[posicao];
+    }
+    
+    public void iniciar() {
+        //....
+        System.out.println("Iniciou o jogo");
+    }
+}
